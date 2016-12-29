22d4cf0ac4215dd9568a8e9b0ea24ab532e3d0e2
Zhenyu Song <zhenyus@mtu.edu>
2016-12-29 14:26:15 +0800

    Debugged: RO321 xpath

--- crawler/crawler/spiders/ro321_spider.py
+++ crawler/crawler/spiders/ro321_spider.py
@@ -1,12 +1,15 @@
 import scrapy
 from crawler.items import RO321Item
 
-class ro321Spider(scrapy.Spider):
+class Ro321Spider(scrapy.Spider):
     name = "ro321"
     allowed_domain = "ro321.com"
-    url_samples = "http://ro321.com/index.php?page=re_item_db&item_id=511"
-
-
+    url_sample = "http://ro321.com/index.php?page=re_item_db&item_id=511"
+    start_urls = [url_sample]
 
     def parse(self, response):
-        xpath = "/html/body/table/tbody/tr[2]/td/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td[2]/center/table[2]/tbody"
+        xpath = "/html/body/table/tr[2]/td/table/tr[2]/td[2]/table/tr[2]/td[2]/center/table[2]"
+        for sel in response.xpath(xpath):
+            item = RO321Item()
+            item["name"] = sel.xpath('tr[1]/td/table/tr/td[2]/b/text()').extract()
+            yield item
